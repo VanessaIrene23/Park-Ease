63c437b0b403b5de8ece458207d254e59d02f1d5
VanessaIrene23 <vanessanamakula79@gmail.com>
2026-05-22 13:18:33 +0300

    add new files

--- vehicles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import vehicle_register, vehicle_list, vehicle_detail
-
-urlpatterns = [
-    path('', vehicle_list, name='vehicle_list'),
-    path('vehicles/register/', vehicle_register, name='vehicle_register'),
-    path('vehicles/<int:pk>/', vehicle_detail, name='vehicle_detail'),
-]
